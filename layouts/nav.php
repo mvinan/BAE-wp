3d637b6d8c337931b148ea48d31cd08477c23be9
Miguel Alejandro <mvinan@icloud.com>
2015-10-07 22:49:48 -0500

    Navigation styles and files structure added for manipulate layouts

--- layouts/nav.php
+++ layouts/nav.php
@@ -1,10 +1,13 @@
-<main class="menu">
+<main>
+  <a href="#" class="icon-times fa fa-times fa-fw"></a>
   <?php wp_nav_menu(
     array(
       'theme_location' => 'nav-header',
       'menu_class' => 'navigation-list',
       'container' => 'nav',
+      'link_before' => '<i class="icon icons-list fa fa-circle-o fa-fw"></i>'
     )
   ); ?>
+
 </main>
-<button name="MENÚ" class="btn btn-default menu-button"><i class="fa fa-bars fa-lg icon"></i>MENÚ</button>
+<button name="MENÚ" class="btn btn-default btn-menu"><i class="fa fa-bars fa-lg icon"></i>MENÚ</button>
