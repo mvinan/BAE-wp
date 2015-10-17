16f33b86ab053598a18743704cfda71fdb01db34
Miguel Alejandro <mvinan@icloud.com>
2015-10-17 08:48:21 -0500

    adding files for news

--- single.php
+++ single.php
@@ -1,26 +1,7 @@
 <?php get_header(); ?>
-<section class="">
-	<article class="single">
-		<div class="container-fluid post_content">
-			<span class="here"></span>
 
-			<div class="the_content row-fluid">
+	<?php include(locate_template('layouts/cover.php')); ?>
 
-				<!-- The Content LOOP-->
-				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
-					<?php the_content(); ?>
-					<?php the_author(); ?>
-					<?php the_author_image(  ); ?>
-
-				<?php endwhile; else : ?>
-					<p><?php _e( 'Lo siento no existe algun contenido en esta single Page.' ); ?></p>
-				<?php endif; ?>
-				<!-- END Content LOOP-->
-
-			</div>
-		</div>
-	</article>
-</section>
+	<?php include(locate_template('layouts/content.php')); ?>
 
 <?php get_footer(); ?>
